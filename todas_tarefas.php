304c9f34a335f3f6aa33d42092549fa5c8763bff
Fernando <fer.developer@outlook.com>
2020-10-07 09:07:39 -0300

    manutenção na tarefa service (recuperar)

--- todas_tarefas.php
+++ todas_tarefas.php
@@ -46,7 +46,7 @@
 
 								<?php foreach($tarefas as $indice => $tarefa) { ?>
 									<div class="row mb-3 d-flex align-items-center tarefa">
-										<div class="col-sm-9"><?php= $tarefa->tarefa ?> (<?php= $tarefa->status ?>)</div>
+										<div class="col-sm-9"><?php=$tarefa->tarefa?> (<?php=$tarefa->status?>)</div>
 										<div class="col-sm-3 mt-2 d-flex justify-content-between">
 											<i class="fas fa-trash-alt fa-lg text-danger"></i>
 											<i class="fas fa-edit fa-lg text-info"></i>
